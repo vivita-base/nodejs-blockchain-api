7d97c915a6aa9aa6a62652e0b0df66cb69d001d3
trevorwood222 <trevorwood222@gmail.com>
2023-01-23 09:49:42 -0600

    up

--- handlers/test.js
+++ handlers/test.js
@@ -2,15 +2,15 @@
 module.exports = (async() => {
 
   // const BitcoreLib = require('bitcore-lib');
-  // const Mnemonic = require('bitcore-mnemonic');
+  const Mnemonic = require('bitcore-mnemonic');
   // const Wallet = require('ethereumjs-wallet');
   // const Web3 = require('web3');
-  const Dawicore = await require('../libs/dawicore');
+  // const Dawicore = await require('../libs/dawicore');
 
-  const {CoinNodesEnum} = await require('../enums/coin-nodes');
-  const {CoinEnum} = require('../enums/coin');
+  // const {CoinNodesEnum} = await require('../enums/coin-nodes');
+  // const {CoinEnum} = require('../enums/coin');
   // const Coin = await require('../funcs/coin/bundle');
-  const {RpcMethodEnum} = require('../enums/rpc-method');
+  // const {RpcMethodEnum} = require('../enums/rpc-method');
 
 
   return {
@@ -20,6 +20,12 @@ module.exports = (async() => {
 
       // === Create a Seed Phrase
       // let wordList = new Mnemonic(Mnemonic.Words.ENGLISH).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.CHINESE).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.FRENCH).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.ITALIAN).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.JAPANESE).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.KOREAN).toString();
+      // let wordList = new Mnemonic(Mnemonic.Words.SPANISH).toString();
       // d.res.wordList = wordList;
 
       // === Create a Wallet using Seed Phrase
